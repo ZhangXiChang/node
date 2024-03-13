009fe4f843da7f9131c00fc383ed91d00188a221
ZhangXiChang <yxrbeigua@outlook.com>
2024-03-13 18:49:40 +0800

    quinn协议分支

--- root-node/src/main.rs
+++ root-node/src/main.rs
@@ -1,21 +1,18 @@
 use std::{fs::File, io::Read, sync::Arc, time::Duration};
 
 use anyhow::Result;
+use bincode::{Decode, Encode};
 use clap::Parser;
 use quinn::{Endpoint, ServerConfig, TransportConfig};
 use tokio::sync::Mutex;
 
-enum Message {
-    Undefined,
-    RequestOnlineNode,
-}
-impl From<Vec<u8>> for Message {
-    fn from(value: Vec<u8>) -> Self {
-        match value[0] {
-            0x00 => Message::RequestOnlineNode,
-            _ => Message::Undefined,
-        }
-    }
+#[derive(Encode, Decode)]
+enum DataPacket {
+    RequestRegisterNode {
+        node_name: String,
+        node_cert: Vec<u8>,
+    },
+    RequestGetOnlineNode,
 }
 
 #[derive(Parser)]
@@ -26,6 +23,12 @@ struct CLIArgs {
     ///私钥文件路径
     #[arg(long)]
     key_path: String,
+}
+
+struct NodeInfo {
+    name: String,
+    cert: Vec<u8>,
+    id: usize,
 }
 
 #[tokio::main]
@@ -48,54 +51,58 @@ async fn main() -> Result<()> {
         "0.0.0.0:10270".parse()?,
     )?;
     println!("根节点创建成功");
-    //在线节点
-    let online_connection = Arc::new(Mutex::new(Vec::new()));
+    //在线节点储存
+    let online_node = Arc::new(Mutex::new(Vec::new()));
     //接收连接
     while let Some(connecting) = endpoint.accept().await {
-        let online_connection = online_connection.clone();
+        let online_node = online_node.clone();
         tokio::spawn(async move {
             let connection = connecting.await?;
-            {
-                let mut online_connection = online_connection.lock().await;
-                online_connection.push(connection.clone());
-            }
             println!("[{}]节点连接成功", connection.remote_address());
             //应答
             loop {
                 match connection.accept_uni().await {
-                    Ok(mut recv) => match Message::from(recv.read_to_end(usize::MAX).await?) {
-                        Message::RequestOnlineNode => match connection.open_uni().await {
-                            Ok(mut send) => {
-                                let online_connection = online_connection.lock().await;
-                                for connection in online_connection.iter() {
-                                    connection
-                                        .peer_identity()
-                                        .unwrap()
-                                        .downcast::<rustls::Certificate>()
-                                        .unwrap();
-                                    send.write_all("".as_bytes()).await?;
+                    Ok(mut recv) => {
+                        let data_packet: DataPacket = bincode::decode_from_slice(
+                            recv.read_to_end(usize::MAX).await?.as_slice(),
+                            bincode::config::standard(),
+                        )?
+                        .0;
+                        match data_packet {
+                            DataPacket::RequestRegisterNode {
+                                node_name,
+                                node_cert,
+                            } => {
+                                let mut online_node = online_node.lock().await;
+                                online_node.push(NodeInfo {
+                                    name: node_name,
+                                    cert: node_cert,
+                                    id: connection.stable_id(),
+                                });
+                            }
+                            DataPacket::RequestGetOnlineNode => {
+                                let online_node = online_node.lock().await;
+                                let mut send = connection.open_uni().await?;
+                                for node_info in online_node.iter() {
+                                    send.write_all(node_info.name.as_bytes()).await?;
                                 }
                                 send.finish().await?;
                             }
-                            Err(_) => (),
-                        },
-                        _ => (),
-                    },
+                        }
+                    }
                     Err(err) => {
                         println!(
                             "[{}]节点断开连接，原因：{}",
                             connection.remote_address(),
                             err
                         );
+                        let mut online_node = online_node.lock().await;
+                        for i in 0..online_node.len() {
+                            if online_node[i].id == connection.stable_id() {
+                                online_node.remove(i);
+                            }
+                        }
                         break;
-                    }
-                }
-            }
-            {
-                let mut online_connection = online_connection.lock().await;
-                for i in 0..online_connection.len() {
-                    if online_connection[i].remote_address() == connection.remote_address() {
-                        online_connection.remove(i);
                     }
                 }
             }
