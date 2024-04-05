34e4e720b2761fe3b71a1227201b87a9b810f1b4
ZhangXiChang <yxrbeigua@outlook.com>
2024-04-05 14:51:41 +0800

    接下来获取窗口位置

--- crate/node/src/main.rs
+++ crate/node/src/main.rs
@@ -3,24 +3,29 @@ use std::{borrow::Cow, sync::Arc};
 use eyre::{eyre, Result};
 
 use eframe::egui;
-use log::error;
+use log::{info, LevelFilter};
 
 const ICON: &[u8] = include_bytes!("../../../assets/icon/node_network_icon.png");
 const ICON_WIDTH: u32 = 512;
 const ICON_HEIGHT: u32 = 512;
 
+#[derive(Default)]
 struct App;
-impl Default for App {
-    fn default() -> Self {
-        Self
-    }
-}
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::SidePanel::left("MenuBar").show(ctx, |ui| {
-            if ui.button("绘制曲线").clicked() {
-                error!("执行绘制曲线");
-            }
+        egui::TopBottomPanel::top("MenuBar").show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                if ui.button("关闭").clicked() {
+                    ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                }
+                let (response, _) = ui.allocate_painter(ui.available_size(), egui::Sense::drag());
+                if response.dragged() {
+                    info!("拖动的距离{:?}", response.drag_delta());
+                    ctx.send_viewport_cmd(egui::ViewportCommand::OuterPosition(egui::Pos2::new(
+                        0., -10.,
+                    )));
+                }
+            });
         });
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add(
@@ -36,31 +41,30 @@ impl eframe::App for App {
 
 fn main() -> Result<()> {
     //初始化日志消费者
-    env_logger::init();
-    //加载应用图标
-    let icon = image::load_from_memory(ICON)?;
+    env_logger::builder().filter_level(LevelFilter::Info).init();
     //运行应用
     eframe::run_native(
         "节点网络",
         eframe::NativeOptions {
             viewport: egui::ViewportBuilder {
                 icon: Some(Arc::new(egui::IconData {
-                    rgba: icon.into_bytes(),
+                    rgba: image::load_from_memory(ICON)?.into_bytes(),
                     width: ICON_WIDTH,
                     height: ICON_HEIGHT,
                 })),
                 inner_size: Some(egui::Vec2::new(
-                    ICON_WIDTH as f32 + 145.,
-                    ICON_HEIGHT as f32,
+                    ICON_WIDTH as f32 + 300.,
+                    ICON_HEIGHT as f32 + 300.,
                 )),
                 resizable: Some(false),
+                decorations: Some(true),
                 ..Default::default()
             },
             ..Default::default()
         },
         Box::new(|cc| {
-            egui_extras::install_image_loaders(&cc.egui_ctx);
             set_font(&cc.egui_ctx);
+            egui_extras::install_image_loaders(&cc.egui_ctx);
             Box::new(App::default())
         }),
     )
@@ -70,20 +74,20 @@ fn main() -> Result<()> {
 fn set_font(ctx: &egui::Context) {
     let mut font_definitions = egui::FontDefinitions::default();
     font_definitions.font_data.insert(
-        "SourceHanSansCN-Bold".to_string(),
+        "SourceHanSansCN-Bold".to_owned(),
         egui::FontData::from_static(include_bytes!(
             "../../../assets/fonts/SourceHanSansCN-Bold.otf"
         )),
     );
     font_definitions
         .families
-        .get_mut(&egui::FontFamily::Proportional)
-        .unwrap()
-        .insert(0, "SourceHanSansCN-Bold".to_string());
+        .entry(egui::FontFamily::Proportional)
+        .or_default()
+        .insert(0, "SourceHanSansCN-Bold".to_owned());
     font_definitions
         .families
-        .get_mut(&egui::FontFamily::Monospace)
-        .unwrap()
-        .push("SourceHanSansCN-Bold".to_string());
+        .entry(egui::FontFamily::Monospace)
+        .or_default()
+        .push("SourceHanSansCN-Bold".to_owned());
     ctx.set_fonts(font_definitions);
 }
